da29cd750b1ecce6a0e86cfbfed5cfb4db200b0f
Karam Qubsi <karamqubsi@gmail.com>
2017-08-14 17:41:18 +0800

    add the ability to add prosperities to index before creating i t

--- src/ElasticsearchProvider.php
+++ src/ElasticsearchProvider.php
@@ -8,17 +8,42 @@ use Elasticsearch\ClientBuilder as ElasticBuilder;
 
 class ElasticsearchProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        app(EngineManager::class)->extend('elasticsearch', function($app) {
-            return new ElasticsearchEngine(ElasticBuilder::create()
-                ->setHosts(config('scout.elasticsearch.hosts'))
-                ->build(),
-                config('scout.elasticsearch.index')
-            );
-        });
+  /**
+  * Bootstrap the application services.
+  */
+  protected $client;
+
+  public function boot()
+  {
+    $this->client = ElasticBuilder::create()
+    ->setHosts(config('scout.elasticsearch.hosts'))
+    ->setSerializer('\Elasticsearch\Serializers\EverythingToJSONSerializer')
+    ->build();
+    /* check for index */
+    $indexpara['index'] = config('scout.elasticsearch.index');
+    $isIndexCreated = $this->client->indices()->exists($indexpara);
+    #if it wasn't created , create it with this sittings.
+    if (!$isIndexCreated) {
+      $params = [
+        'index' => config('scout.elasticsearch.index'),
+        'body' => [
+          'settings' => [
+            'number_of_shards' => 3,
+            'number_of_replicas' => 2
+          ]
+        ]
+      ];
+      // Create the index with mappings and settings now
+      $response = $this->client->indices()->create($params);
+
     }
+
+
+
+    app(EngineManager::class)->extend('elasticsearch', function($app) {
+      return new ElasticsearchEngine($this->client,
+      config('scout.elasticsearch.index')
+    );
+  });
+}
 }
